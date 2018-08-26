5e130923ca716731d7cd2455bb0bec9bd115313e
Daniele Ambrosino <mail@danieleambrosino.it>
2018-08-26 14:36:09 +0200

    Implemented Echoer class

--- core/observers/Echoer.php
+++ core/observers/Echoer.php
@@ -1,22 +1,38 @@
 <?php
 
-/**
+/*
+ * This file is part of the RacFiNordBot package.
  * 
+ * (c) 2018 Daniele Ambrosino <mail@danieleambrosino.it>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file distributed with this source code.
  */
-class Echoer extends Communicator {
 
-    /**
-     * @param string text
-     */
-    protected function sendMessage(string $text) {
-        // TODO implement here
-    }
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
 
-    /**
-     * 
-     */
-    protected function sendIsTyping() {
-        // TODO implement here
-    }
+/**
+ * Class that echoes bot's responses.
+ * 
+ * @author Daniele Ambrosino <mail@danieleambrosino.it>
+ */
+class Echoer extends Communicator
+{
+
+  /**
+   * @param string text
+   */
+  protected function sendMessage(string $text)
+  {
+    echo $text . PHP_EOL;
+  }
+
+  /**
+   * 
+   */
+  protected function sendIsTyping()
+  {
+    echo 'The bot is computing your answer, please wait' . PHP_EOL;
+  }
 
 }
