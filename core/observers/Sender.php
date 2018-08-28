39f43ae995e93e1a6a9bf99bcdbae8b8f709186f
Daniele Ambrosino <mail@danieleambrosino.it>
2018-08-28 18:17:51 +0200

    Change copyright header

--- core/observers/Sender.php
+++ core/observers/Sender.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the RacFiNordBot package.
  * 
- * (c) 2018 Daniele Ambrosino <mail@danieleambrosino.it>
+ * (c) 2018 Rotaract Club Firenze Nord <rotaractfirenzenord@gmail.com>
  * 
  * For the full copyright and license information, please view the LICENSE
  * file distributed with this source code.
